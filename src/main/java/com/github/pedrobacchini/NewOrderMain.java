780e80d28f618ca4899f44f958c3dae64724ca23
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-02 20:24:06 -0400

    multiply consumer and producer

--- src/main/java/com/github/pedrobacchini/NewOrderMain.java
+++ src/main/java/com/github/pedrobacchini/NewOrderMain.java
@@ -1,5 +1,6 @@
 package com.github.pedrobacchini;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,14 +14,18 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
         var value = "pedido02,100";
+        var email = "Thank you for your order! We are processing your order";
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record, (data, ex) -> {
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        Callback callback = (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
             }
             System.out.println("sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/" + data.timestamp());
-        }).get();
+        };
+        producer.send(record, callback).get();
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
