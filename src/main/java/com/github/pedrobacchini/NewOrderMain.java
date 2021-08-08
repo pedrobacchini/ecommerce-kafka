2c503f4c426b87347aa1b8857c872d3c29d8337e
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-08 09:22:07 -0400

    handle with object serializer

--- src/main/java/com/github/pedrobacchini/NewOrderMain.java
+++ src/main/java/com/github/pedrobacchini/NewOrderMain.java
@@ -1,5 +1,6 @@
 package com.github.pedrobacchini;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -7,12 +8,18 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        try(KafkaDispatcher kafkaDispatcher = new KafkaDispatcher()) {
-            var key = UUID.randomUUID().toString();
-            var value = key + ",100";
-            kafkaDispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+        var userId = UUID.randomUUID().toString();
+
+        try (var kafkaDispatcher = new KafkaDispatcher<Order>()) {
+            var orderId = UUID.randomUUID().toString();
+            var amount = BigDecimal.valueOf(Math.random() * 50000 + 1);
+            Order order = new Order(userId, orderId, amount);
+            kafkaDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+        }
+
+        try (var kafkaDispatcher = new KafkaDispatcher<String>()) {
             var email = "Thank you for your order! We are processing your order";
-            kafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", email, email);
+            kafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
         }
     }
 }
