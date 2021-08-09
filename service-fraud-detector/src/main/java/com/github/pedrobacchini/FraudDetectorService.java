e2462ac46e3e942c338d8f8e8ed58f423f0e3533
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-08 20:37:55 -0400

    create modules for ecommerce service's

--- service-fraud-detector/src/main/java/com/github/pedrobacchini/FraudDetectorService.java
+++ service-fraud-detector/src/main/java/com/github/pedrobacchini/FraudDetectorService.java
@@ -2,7 +2,6 @@ package com.github.pedrobacchini;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.util.Map;
 
