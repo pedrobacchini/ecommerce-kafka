828429b5172cd0b408f9329d59ae4b706d236a1a
Pedro Bacchini <pedrobacchini@outlook.com>
2021-08-09 23:18:20 -0400

    add email at Order and execute frand and CreateUserService in parallel

--- service-new-order/src/main/java/com/github/pedrobacchini/NewOrderMain.java
+++ service-new-order/src/main/java/com/github/pedrobacchini/NewOrderMain.java
@@ -8,18 +8,21 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        var userId = UUID.randomUUID().toString();
+        var email = Math.random()+"@email.com";
 
-        try (var kafkaDispatcher = new KafkaDispatcher<Order>()) {
-            var orderId = UUID.randomUUID().toString();
-            var amount = BigDecimal.valueOf(Math.random() * 50000 + 1);
-            Order order = new Order(userId, orderId, amount);
-            kafkaDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
-        }
+        for (int i = 0; i < 10; i++) {
+            var userId = UUID.randomUUID().toString();
+            try (var kafkaDispatcher = new KafkaDispatcher<Order>()) {
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 50000 + 1);
+                Order order = new Order(userId, orderId, amount, email);
+                kafkaDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+            }
 
-        try (var kafkaDispatcher = new KafkaDispatcher<String>()) {
-            var email = "Thank you for your order! We are processing your order";
-            kafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+            try (var kafkaDispatcher = new KafkaDispatcher<String>()) {
+                var emailCode = "Thank you for your order! We are processing your order";
+                kafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, emailCode);
+            }
         }
     }
 }
--- service-user/src/main/java/com/github/pedrobacchini/CreateUserService.java
+++ service-user/src/main/java/com/github/pedrobacchini/CreateUserService.java
@@ -15,9 +15,14 @@ public class CreateUserService {
     CreateUserService() throws SQLException {
         var url = "jdbc:sqlite:users_database.db";
         this.connection = DriverManager.getConnection(url);
-        connection.createStatement().execute(
-                "create table Users(uuid varchar(200) primary key, email varchar(200))"
-        );
+        try {
+            connection.createStatement().execute(
+                    "create table Users(uuid varchar(200) primary key, email varchar(200))"
+            );
+        } catch (SQLException ex) {
+            //be careful, the sql could be wrong, be realllllly careful
+            ex.printStackTrace();
+        }
     }
 
     public static void main(String[] args) throws SQLException {
@@ -28,7 +33,7 @@ public class CreateUserService {
                 GsonDeserializer.TYPE_CONFIG, Order.class.getName()
         );
         try (var service = new KafkaService<>(
-                "ECOMMERCE_ORDER_APPROVED",
+                "ECOMMERCE_NEW_ORDER",
                 createUserService::parse,
                 overrideProperties)) {
             service.run();
@@ -41,16 +46,16 @@ public class CreateUserService {
         System.out.println(record.value());
         var order = record.value();
         if(isNewUser(order.getEmail())) {
-            insertNewUser(order.getEmail());
+            insertNewUser(order.getUserId(), order.getEmail());
         }
     }
 
-    private void insertNewUser(String email) throws SQLException {
+    private void insertNewUser(String uuid, String email) throws SQLException {
         var insert = connection.prepareStatement("insert into Users (uuid, email) values (?,?)");
-        insert.setString(1, "uuid");
+        insert.setString(1, uuid);
         insert.setString(2, email);
         insert.execute();
-        System.out.println("User uuid is "+email);
+        System.out.println("Creating user with email "+email);
     }
 
     private boolean isNewUser(String email) throws SQLException {
